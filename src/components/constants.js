2f74102306fdf9280a0093b99ec587ff0a499eca
onetimecircle <lerners124@gmail.com>
2023-03-15 13:03:38 -0400

    components and their respective stylesheets

---
+++ src/components/constants.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../css/constants.css'
+
+
+export class Page extends React.Component {
+    constructor(){
+        super()
+    }
+
+    render(){
+
+       return <div>
+        <div className="page">
+            <h1 className="title">Cv-Maker</h1>
+        </div>
+        </div>
+    }
+}
---
+++ src/components/viewToggler.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Toggler extends React.Component {
+    constructor(){
+        super()
+    }
+
+    render(){
+        
+    }
+}
