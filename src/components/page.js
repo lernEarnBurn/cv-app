56ed7aff0ad0133d95166c78e3d0438586dc6272
onetimecircle <lerners124@gmail.com>
2023-03-16 11:38:26 -0400

    toggles between edit envirement and view

--- src/components/page.js
+++ src/components/page.js
@@ -5,19 +5,44 @@ import { Section } from "./section";
 
 
 export class Page extends React.Component {
+    constructor(){
+        super()
+        this.state = {viewMode : false}
+        this.isViewMode = this.isViewMode.bind(this)
+    }
+
+    isViewMode = (bool) => {
+        this.setState({ viewMode: bool });
+      }
+
     
     render(){
 
-        return   <div className="main-container">
-                    <Toggler/>
-                    <div className="page">
-                        <div className="background-container">
-                            <h1 className="title">CV-Maker</h1>
-                        </div>
-                        <Section header="General Information" name1="First Name" name2="Last Name" name3="Email" name4="Phone Number"/>
-                        <Section header="Educational Experience" name1="School Name" name2="Title of Study" type1="date" type2="date"/> 
-                        <Section header="Practical Experience" name1="Company Name" name2="Position" type1="date" type2="date"/> 
+        return <div>{(!this.state.viewMode) ? 
+            <div className="main-container">
+                <Toggler isViewMode={this.isViewMode}/>
+                <div className="page">
+                    <div className="background-container">
+                        <h1 className="title">CV-Maker</h1>
+                    </div>
+                    <Section header="General Information" name1="First Name" name2="Last Name" name3="Email" name4="Phone Number"/>
+                    <Section header="Educational Experience" name1="School Name" name2="Title of Study" type1="date" type2="date"/> 
+                    <Section header="Practical Experience" name1="Company Name" name2="Position" type1="date" type2="date"/> 
+                </div>
+            </div> 
+
+            :
+
+            <div className="main-container">
+                <Toggler isViewMode={this.isViewMode}/>
+                <div className="page">
+                    <div className="background-container">
+                        <h1 className="title">CV-Maker</h1>
                     </div>
                 </div>
+            </div>
+        }</div>
+
+    
     }
 }
--- src/components/viewToggler.js
+++ src/components/viewToggler.js
@@ -1,16 +1,35 @@
 import React from "react";
 import '../css/viewToggler.css'
 
+function processClasses(nonTargetClass, event){
+    event.target.classList.add("active")
+    event.target.classList.remove("inactive")
+    document.querySelector(nonTargetClass).classList.add("inactive")
+    document.querySelector(nonTargetClass).classList.remove("active")
+}
+
 export class Toggler extends React.Component {
-    constructor(){
-        super()
-        this.state = {viewMode : false}
+    constructor(props){
+        super(props)
+        this.toggleMode = this.toggleMode.bind(this)
+    }
+
+    toggleMode(event){
+        const { isViewMode } = this.props
+
+        if(event.target.classList.contains("update")){
+            processClasses('.view', event)  
+            isViewMode(false)
+        }else if(event.target.classList.contains("view")){
+            processClasses('.update', event)
+            isViewMode(true)
+        }
     }
 
     render(){
         return <div className="toggler">
-            <div className="half update active">Update</div>
-            <div className="half view inactive">View</div>
+            <div onClick={this.toggleMode} className="half update active">Update</div>
+            <div onClick={this.toggleMode} className="half view inactive">View</div>
         </div>
     }
 }
